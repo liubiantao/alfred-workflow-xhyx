2b006dd8cd9cad77923376c66a1ccca8dfc964d4
haitao.yin <haitao.yin@alo7.com>
2020-03-20 13:00:25 +0800

    fix: 查询接口失效，改为本地查询

--- index.js
+++ index.js
@@ -1,57 +1,45 @@
-const alfy = require('alfy')
-const FormData = require('form-data')
+import alfy from "alfy";
+import xhyx from "./xhyx.json";
 
-const form = new FormData()
-form.append('search_word', alfy.input)
-
-const rawData = await alfy.fetch(
-  'http://www.xhup.club/Xhup/Search/searchCode',
-  {
-    method: 'POST',
-    body: form,
-    json: false
-  }
-)
-
-const data = JSON.parse(rawData)
+const input = String(alfy.input).charAt(0)
+const data = xhyx[input];
 
 const getItems = () => {
-  if (data.noResultCount > 0) {
+  if (data === "未收录") {
     return [
       {
-        title: `${alfy.input}：    未收录的字`,
-        subtitle: '非《通用规范汉字表》国发〔2013〕23号文规定用字，故未收录',
-        arg: alfy.input
+        title: `${input}：    未收录的字`,
+        subtitle: "非《通用规范汉字表》国发〔2013〕23号文规定用字，故未收录",
+        arg: input
       }
-    ]
-  } else if (data.list_dz.length > 0) {
-    const list = data.list_dz[0]
+    ];
+  } else if (data && data.length > 0) {
     return [
       {
-        title: list[0],
-        arg: list[6]
+        title: data[0],
+        arg: input
       },
       {
-        title: `拆　分：     ${list[1]}`,
-        arg: list[6]
+        title: `拆　分：     ${data[1]}`,
+        arg: input
       },
       {
-        title: `首　末：     ${list[2]}     ${list[3]}`,
-        arg: list[6]
+        title: `首　末：     ${data[2]}     ${data[3]}`,
+        arg: input
       },
       {
-        title: `编　码：     ${list[4]}     ${list[5]}`,
-        arg: list[6]
+        title: `编　码：     ${data[4]}     ${data[5]}`,
+        arg: input
       }
-    ]
+    ];
   } else {
     return [
       {
-        title: '请输入汉字',
-        arg: alfy.input
+        title: "查询错误",
+        arg: input
       }
-    ]
+    ];
   }
-}
+};
 
-alfy.output(getItems())
+alfy.output(getItems());
